30ac78715ad1f24edf8b6d90d0171104113069bc
hamzajutt506 <sp19-bcs-051@cuilahore.edu.pk>
2024-05-19 18:41:05 +0500

    main

--- script/script.js
+++ script/script.js
@@ -2,3 +2,28 @@ $(".header-hamburger").click(function () {
   $(this).toggleClass("active");
   $(".header-menu").toggleClass("active");
 });
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+// Function to check scroll position
+function checkScroll() {
+  if (window.scrollY > 500) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+// Function to scroll to the top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Add event listener to show/hide the button on scroll
+window.addEventListener("scroll", checkScroll);
+
+// Add event listener to scroll to top on button click
+scrollToTopButton.addEventListener("click", scrollToTop);
